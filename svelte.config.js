167bb002a20b619853298a5309a0d1bc971c030d
Paillaugue Angus <angus.paillaugue@etu.iut-tlse3.fr>
2023-12-30 18:17:55 +0100

    Changed adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js'; 
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 
 /** @type {import('@sveltejs/kit').Config} */
