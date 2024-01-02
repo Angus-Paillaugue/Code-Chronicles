3f3db4c568acbea936318640bc4fc17088c3d304
Paillaugue Angus <angus.paillaugue@etu.iut-tlse3.fr>
2024-01-02 20:04:54 +0100

    Fix for dynamic import of post #4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import dynamicImport from 'vite-plugin-dynamic-import'
+import dynamicImport from 'vite-plugin-dynamic-import';
 
 
 export default defineConfig({
