2f72afc6401417034a1473ad8a91633788353fa7
Paillaugue Angus <angus.paillaugue@etu.iut-tlse3.fr>
2024-01-02 19:51:00 +0100

    Dynamic import fix #3

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dynamicImport from 'vite-plugin-dynamic-import'
+
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    dynamicImport(),
+  ],
 });
