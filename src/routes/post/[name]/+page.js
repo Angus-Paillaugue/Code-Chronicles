95d10c3b44052e0af2d7fa9f1bb32f97ef079d5b
Paillaugue Angus <angus.paillaugue@etu.iut-tlse3.fr>
2024-01-02 19:41:58 +0100

    Test fix for file dynamic import

--- src/routes/post/[name]/+page.js
+++ src/routes/post/[name]/+page.js
@@ -1,6 +1,6 @@
 export async function load({ data }) {
   // Since the path is dynamic, who knows what the name will be?
-  const component = await import(/* @vite-ignore */`/src/lib/posts/${data.post.slug}.md`);
+  const component = await import(/* @vite-ignore */`../../../lib/posts/${data.post.slug}.md`);
   
   return {
     component: component.default,
